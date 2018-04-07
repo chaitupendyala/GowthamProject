0aedcee6ed9b054947ec0a75545f3fe902ce66d5
chaitupendyala <prndyala@gmail.com>
2018-04-07 12:04:07 +0530

    added your code also

--- Retinopathy/views.py
+++ Retinopathy/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render,HttpResponse, redirect
 from django.views.decorators.csrf import csrf_exempt
 from . import models
+import cv2
+import numpy as np
+import keras
+from keras.models import load_model
 
-@csrf_exempt
+model = load_model("G:\DR_Two_Classes_recall_0.7759.h5")
+
 def enter_image(request):
     return render(request,"Retinopathy/main.html",{})
 
@@ -11,6 +16,15 @@ def store_image(request):
     if request.method == "POST":
         u = models.Eye_Images(image = request.FILES['image'])
         u.save()
-        return redirect("/check")
+        return HttpResponse("The Value is "+evaluate(u.id))
     else:
         return redirect("/check")
+
+def evaluate(id_of_object):
+    image_path = str(models.Eye_Images.get(id = id_of_object).path)
+    image = cv2.imread(image_path)
+    #image = cv2.resize(image,(256,256))
+    image = image / 255
+    image = np.expand_dims(image, axis=0)
+    score = model.predict(image)
+    return score*100

ab272e7e6ff43670a635c2cec8e9a42574f089ec
chaitupendyala <prndyala@gmail.com>
2018-04-07 19:52:50 +0530

    Now the path is shown

--- Retinopathy/views.py
+++ Retinopathy/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render,HttpResponse, redirect
 from django.views.decorators.csrf import csrf_exempt
 from .models import Eye_Images
-#import cv2
+import cv2
 import numpy as np
-#import keras
-#from keras.models import load_model
+import keras
+from keras.models import load_model
 
-#model = load_model("G:\DR_Two_Classes_recall_0.7759.h5")
+model = load_model("G:\DR_Two_Classes_recall_0.7759.h5")
 
 def enter_image(request):
     return render(request,"Retinopathy/main.html",{})
@@ -14,7 +14,7 @@ def enter_image(request):
 @csrf_exempt
 def store_image(request):
     if request.method == "POST":
-        u = models.Eye_Images(image = request.FILES['image'])
+        u = Eye_Images(image = request.FILES['image'])
         u.save()
         return_val = evaluate(u.id)
         return HttpResponse("<h1>Class 1 resemblence is :"+return_val[0]+"<br/>"+"Class 2 resemblence is :"+return_val[1])
@@ -22,9 +22,9 @@ def store_image(request):
         return redirect("/check")
 
 def evaluate(id_of_object):
-    image_path = str(Eye_Images.objects.get(id = id_of_object).path)
+    image_path = str(Eye_Images.objects.get(id = id_of_object).image.path)
     image = cv2.imread(image_path)
-    #image = cv2.resize(image,(256,256))
+    image = cv2.resize(image,(256,256))
     image = image / 255
     image = np.expand_dims(image, axis=0)
     score = model.predict(image)

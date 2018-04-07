119436a9b4a92a7297653f64f3fdec13c10d143c
chaitupendyala <prndyala@gmail.com>
2018-04-07 19:23:42 +0530

    without erros

--- Retinopathy/views.py
+++ Retinopathy/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render,HttpResponse, redirect
 from django.views.decorators.csrf import csrf_exempt
-from . import models
-import cv2
+from .models import Eye_Images
+#import cv2
 import numpy as np
-import keras
-from keras.models import load_model
+#import keras
+#from keras.models import load_model
 
-model = load_model("G:\DR_Two_Classes_recall_0.7759.h5")
+#model = load_model("G:\DR_Two_Classes_recall_0.7759.h5")
 
 def enter_image(request):
     return render(request,"Retinopathy/main.html",{})
@@ -16,12 +16,13 @@ def store_image(request):
     if request.method == "POST":
         u = models.Eye_Images(image = request.FILES['image'])
         u.save()
-        return HttpResponse("The Value is "+evaluate(u.id))
+        return_val = evaluate(u.id)
+        return HttpResponse("<h1>Class 1 resemblence is :"+return_val[0]+"<br/>"+"Class 2 resemblence is :"+return_val[1])
     else:
         return redirect("/check")
 
 def evaluate(id_of_object):
-    image_path = str(models.Eye_Images.get(id = id_of_object).path)
+    image_path = str(Eye_Images.objects.get(id = id_of_object).path)
     image = cv2.imread(image_path)
     #image = cv2.resize(image,(256,256))
     image = image / 255
